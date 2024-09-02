eb07b512392be3c356f4092d44bd7dd935ce4422
Felix <bartlf@htw-berlin.de>
2024-09-02 12:11:12 +0200

    initial commit

---
+++ website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vorlage(request):
+    return render(request, 'vorlage.html')
+def buchungsaufgabe(request):
+    return render(request, 'buchungsaufgabe.html')
+def index(request):
+    return render(request, 'index.html')
+def login(request):
+    return render(request, 'login.html')
+def themenkomplex1(request):
+    return render(request, 'themenkomplex1.html')
+def themenkomplex2(request):
+    return render(request, 'themenkomplex2.html')
+def themenkomplex3(request):
+    return render(request, 'themenkomplex3.html')
