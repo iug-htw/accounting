7d5509392bafb4c1942be4aa059ad4658fb34e73
571482 <felixxb.96@gmail.com>
2024-09-06 13:51:57 +0200

    settings fix

--- young_wolf/settings.py
+++ young_wolf/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR / 'assets'
-MEDIA_ROOT = BASE_DIR, 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR, 'static'
