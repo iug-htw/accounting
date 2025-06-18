a544c97183fe62cf9d8e88d3d6d981debf136901
Felix <bartlf@htw-berlin.de>
2025-06-18 11:33:23 +0200

    fettig

--- posts/forms.py
+++ posts/forms.py
@@ -118,7 +118,7 @@ class AufgabeImportForm(forms.ModelForm):
 class AufgabeBearbeitenForm(forms.ModelForm):
     class Meta:
         model = Aufgabe_neu
-        fields = ['unternehmen_kategorie', 'fragentyp', 'fragentyp_text', 'rechnungstyp','mailtext','verabschiedung','nutzungsdauer','rechnungsnummer','umsatzsteuerfrei','beschreibung', 'feedback_konto_falsch', 'feedback_betrag_falsch','zahlweise']
+        fields = ['unternehmen_kategorie', 'fragentyp', 'fragentyp_text', 'rechnungstyp','mailtext','verabschiedung','nutzungsdauer','rechnungsnummer','umsatzsteuerfrei','eigene_ansicht','beschreibung', 'feedback_konto_falsch', 'feedback_betrag_falsch','zahlweise']
     
 
 class AufgabeDetailBearbeitenForm(forms.ModelForm):
