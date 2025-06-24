6c0ca3e1a12ffb832c206fbc7bf6947d875bde6e
571482 <felixxb.96@gmail.com>
2025-06-24 18:20:03 +0200

    kleine css fixex, logik für securenet2 drin zwecks orgamails, .po verbessert

--- users/views.py
+++ users/views.py
@@ -485,7 +485,7 @@ def send_willkommen_mail(user):
     ) 
 
 def sende_orga_mail_wenn_noetig(student):
-    if not student.unternehmen or student.unternehmen.id != 1:
+    if student.unternehmen.name not in  ('SecureNet', 'SecureNet_en'):
         return  # Nur für Unternehmen mit ID 1
 
     bereits_geschickt = Mail.objects.filter(
