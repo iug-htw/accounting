699d2682f782afdfeb33183e3df8779af9b3bd5a
571482 <felixxb.96@gmail.com>
2025-08-28 09:30:12 +0200

    GuV/Bilanz in Übersicht für Lehrende sichtbar

--- config/views.py
+++ config/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.shortcuts import redirect
 from collections import defaultdict
 from operator import attrgetter
+from posts.models import NutzerAbschluss
 
 @login_required(login_url="/users/login/")
 def index(request):
@@ -14,18 +15,32 @@ def index(request):
     if request.user.role == 'teacher':
         students = request.user.students.all().select_related('semester', 'studiengang')
 
-        # Gruppierung: {semester_name: {studiengang_name: [students]}}
         grouped_students = defaultdict(lambda: defaultdict(list))
 
         for student in students:
             semester_name = student.semester.name if student.semester else "Unbekanntes Semester"
             studiengang_name = student.studiengang.name if student.studiengang else "Unbekannter Studiengang"
-            grouped_students[semester_name][studiengang_name].append(student)
 
-        # Optional sortieren
-        grouped_students = dict(sorted(grouped_students.items()))  # Semester sortieren
+            # GuV/Bilanz Status abrufen
+            abschluss_info = {}
+            for typ in ["GUV", "BILANZ"]:
+                abschluss = NutzerAbschluss.objects.filter(nutzer=student, typ=typ).first()
+                if abschluss is None or abschluss.korrekt is None:
+                    abschluss_info[typ] = None   # nicht bearbeitet
+                elif abschluss.korrekt:
+                    abschluss_info[typ] = 1      # korrekt
+                else:
+                    abschluss_info[typ] = 0      # falsch
+
+            # Student + Info speichern
+            grouped_students[semester_name][studiengang_name].append({
+                "student": student,
+                "abschluss": abschluss_info
+            })
+
+        grouped_students = dict(sorted(grouped_students.items()))
         for semester in grouped_students:
-            grouped_students[semester] = dict(sorted(grouped_students[semester].items()))  # Studiengänge sortieren
+            grouped_students[semester] = dict(sorted(grouped_students[semester].items()))
 
     return render(request, 'index.html', {'grouped_students': grouped_students})
 def frontpage(request):
